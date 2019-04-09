d6ec71dc0192b8da7f849fb873a12f464701c24e
Austin Casey <thepoast@gmail.com>
2019-04-08 21:32:18 -0500

    wip2

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -1,7 +1,13 @@
-import util from '../helpers/util.js' 
+import util from '../helpers/util.js';
+import book from '../helpers/book.js';
+
 
 const makeStore = () =>{
-    let domString = '<h2>Guh</h2>';
+    const bookInfo = book.getBook();
+    let domString = '<h2>Our Only Book</h2>';
+    domString += `<h3>${bookInfo.price}</h3>`;
+    domString += `<img src='${bookInfo.image} alt='book cover'`;
+    domString += `<button class='btn btn-danger'>Add to cart</button>`
     util.printToDom('store-container', domString);
 };
 
